b74f851a0375206d359742a26da709c9198ca31a
alex-smith-26 <85772857+alex-smith-26@users.noreply.github.com>
2021-06-12 17:02:25 -0400

    First Level + prefab modifications

--- Assets/Scripts/DamageWall.cs
+++ Assets/Scripts/DamageWall.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class DamageWall : MonoBehaviour
@@ -10,6 +11,13 @@ public class DamageWall : MonoBehaviour
         if (col.gameObject.tag == "Ship")
         {
             Destroy(col.gameObject);
+            StartCoroutine("ResetScene");
         }
+    }
+
+    IEnumerator ResetScene()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
