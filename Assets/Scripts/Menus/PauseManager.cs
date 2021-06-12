4e0b564ea64eb38b4bc9f17863408652e92b0e2b
Dmcdominic <dmcdominic@gmail.com>
2021-06-12 16:19:16 -0400

    Added main menu and pause system

---
+++ Assets/Scripts/Menus/PauseManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour {
+
+	public static PauseManager instance;
+	public static bool paused;
+
+	public GameObject pauseMenu;
+
+
+	// init
+	void Awake() {
+		if (instance && instance != this) {
+			Destroy(gameObject);
+			return;
+		}
+		instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		// If we're in the main menu, can't pause
+		if (SceneManager.GetActiveScene().buildIndex == 0) {
+			return;
+		}
+
+		if ((Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame) ||
+			(Gamepad.current != null && Gamepad.current.startButton.wasPressedThisFrame)) {
+			togglePause();
+		}
+	}
+
+	public void togglePause() {
+		setPaused(!paused);
+	}
+
+	public void setPaused(bool shouldBePaused) {
+		paused = shouldBePaused;
+		Time.timeScale = (paused) ? 0 : 1.0f;
+		pauseMenu.SetActive(paused);
+	}
+
+	public void loadMainMenu() {
+		setPaused(false);
+		SceneManager.LoadScene(0);
+	}
+}
