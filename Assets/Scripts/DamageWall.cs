5e32dd4143e5dc0749da8911c2f4f74cb7c695f9
alex-smith-26 <85772857+alex-smith-26@users.noreply.github.com>
2021-06-12 14:48:51 -0400

    Added walls for level building

---
+++ Assets/Scripts/DamageWall.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageWall : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ship")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
