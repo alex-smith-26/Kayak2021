2107710fc5f9495ee1bf081b39875a90a6d38c77
oberon99 <qpmzwomx@gmail.com>
2021-06-12 18:12:10 -0400

    Line-of-Sight support for enemies & framework for different AI behaviors

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public bool hasAggroOnPlayer = false;
+    public bool hasSightlineOnPlayer = false;
+    public GameObject[] nodes;
     public GameObject player;
+    [SerializeField] public enum AIType {Chase, Kamikaze, Cautious};
+    public AIType type;
     public float rotationFollowStrength;
     public float speed;
     private Rigidbody2D rb2d;
-    float CurrentRotation;
     Vector3 VectorToPlayer;
     float angleToPlayer;
+    private Vector2 desiredPosition;
+
+    void FaceTowardsPlayer()
+    {
+        if (Mathf.Abs(rb2d.rotation - angleToPlayer) < 180)
+        {
+            rb2d.rotation -= (rb2d.rotation - angleToPlayer) * (rotationFollowStrength / 100);
+            print("modRotDiff = " + (rb2d.rotation - angleToPlayer));
+        }
+        else if (rb2d.rotation - angleToPlayer < -180)
+        {
+            rb2d.rotation -= (360 + (rb2d.rotation - angleToPlayer)) * (rotationFollowStrength / 100);
+            print("modRotDiff = " + 360 + (rb2d.rotation - angleToPlayer));
+        }
+        else if (rb2d.rotation - angleToPlayer > 180)
+        {
+            rb2d.rotation -= (rb2d.rotation - (angleToPlayer + 360)) * (rotationFollowStrength / 100);
+            print("modRotDiff = " + (rb2d.rotation - (angleToPlayer + 360)));
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,21 +47,47 @@ public class EnemyAI : MonoBehaviour
     {
         VectorToPlayer = player.transform.position - rb2d.transform.position;
         angleToPlayer = Vector3.SignedAngle(new Vector2(0.0f, 1.0f), VectorToPlayer, Vector3.forward);
-        if (Mathf.Abs(rb2d.rotation - angleToPlayer) < 180)
+        RaycastHit2D lineOfSight = Physics2D.Raycast(transform.position, VectorToPlayer);
+        print("Rigidbody collider is " + lineOfSight.collider);
+        Debug.DrawLine(transform.position, player.transform.position, Color.red);
+        if(lineOfSight.collider != null)
         {
-            rb2d.rotation -= (rb2d.rotation - angleToPlayer) * (rotationFollowStrength / 100);
-            print("modRotDiff = " + (rb2d.rotation - angleToPlayer));
+            if(lineOfSight.collider.gameObject == player)
+            {
+                hasSightlineOnPlayer = true;
+                hasAggroOnPlayer = true;
+            }
+            else
+            {
+                hasSightlineOnPlayer = false;
+                rb2d.velocity -= rb2d.velocity * .75f;
+            }
         }
-        else if (rb2d.rotation-angleToPlayer < -180)
+        if (hasAggroOnPlayer)
         {
-            rb2d.rotation -= (360+(rb2d.rotation-angleToPlayer)) * (rotationFollowStrength / 100);
-            print("modRotDiff = " + 360 + (rb2d.rotation - angleToPlayer));
+            if (hasSightlineOnPlayer)
+            {
+                switch (type)
+                {
+                    case AIType.Chase:
+                        {
+
+                            break;
+                        }
+                    case AIType.Cautious:
+                        {
+                            break;
+                        }
+                    case AIType.Kamikaze:
+                        {
+                            FaceTowardsPlayer();
+                            rb2d.velocity = (VectorToPlayer.normalized) * speed;
+                            break;
+                        }
+                }
+            }
         }
-        else if (rb2d.rotation - angleToPlayer > 180)
-        {
-            rb2d.rotation -= (rb2d.rotation - (angleToPlayer+360)) * (rotationFollowStrength / 100);
-            print("modRotDiff = " + (rb2d.rotation - (angleToPlayer + 360)));
-        }
-        rb2d.transform.position += (VectorToPlayer.normalized) * speed/100;
+        
+        
     }
 }
