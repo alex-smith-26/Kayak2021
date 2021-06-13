1ddf2f4688fa7962ccebcede2b2c88c3a079b8e7
RedHippi <32603448+RedHippi@users.noreply.github.com>
2021-06-13 13:13:11 -0400

    AudioController hooked in

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -3,69 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+namespace UnityCore
+{
+	namespace Audio
+    {
+		public class MenuManager : MonoBehaviour
+		{
 
-public class MenuManager : MonoBehaviour {
+			public GameObject MainMenu;
+			public GameObject ShipSelect;
+			public GameObject LevelSelect;
+			public GameObject Credits;
 
-	public GameObject MainMenu;
-	public GameObject ShipSelect;
-	public GameObject LevelSelect;
-	public GameObject Credits;
+			public AudioController controller;
+
+			// Init
+			void Awake()
+			{
+				ShowMainMenu();
+				controller = GameObject.Find("MusicPlayer").GetComponent<AudioController>();
+			}
 
 
-	// Init
-	void Awake() {
-		ShowMainMenu();
-	}
-
-
-	public void Quit() {
+			public void Quit()
+			{
 #if UNITY_EDITOR
-		UnityEditor.EditorApplication.isPlaying = false;
+				UnityEditor.EditorApplication.isPlaying = false;
 #else
          Application.Quit();
 #endif
-	}
+			}
 
 
-	public void ShowMainMenu() {
-		MainMenu.SetActive(true);
-		ShipSelect.SetActive(false);
-		LevelSelect.SetActive(false);
-		Credits.SetActive(false);
-	}
+			public void ShowMainMenu()
+			{
+				MainMenu.SetActive(true);
+				ShipSelect.SetActive(false);
+				LevelSelect.SetActive(false);
+				Credits.SetActive(false);
+			}
 
-	public void ShowShipSelect() {
-		MainMenu.SetActive(false);
-		ShipSelect.SetActive(true);
-		LevelSelect.SetActive(false);
-		Credits.SetActive(false);
-	}
+			public void ShowShipSelect()
+			{
+				MainMenu.SetActive(false);
+				ShipSelect.SetActive(true);
+				LevelSelect.SetActive(false);
+				Credits.SetActive(false);
+			}
 
-	public void ShowLevelSelect() {
-		MainMenu.SetActive(false);
-		ShipSelect.SetActive(false);
-		LevelSelect.SetActive(true);
-		Credits.SetActive(false);
-	}
+			public void ShowLevelSelect()
+			{
+				MainMenu.SetActive(false);
+				ShipSelect.SetActive(false);
+				LevelSelect.SetActive(true);
+				Credits.SetActive(false);
+			}
 
-	public void ShowCredits() {
-		MainMenu.SetActive(false);
-		ShipSelect.SetActive(false);
-		LevelSelect.SetActive(false);
-		Credits.SetActive(true);
-	}
+			public void ShowCredits()
+			{
+				MainMenu.SetActive(false);
+				ShipSelect.SetActive(false);
+				LevelSelect.SetActive(false);
+				Credits.SetActive(true);
+			}
 
-	// Set a certain type of ship
-	public void SelectShip(int shipType) {
-		ShipSpawner.currentShipType = (ShipType)shipType;
-		ShowLevelSelect();
-	}
+			// Set a certain type of ship
+			public void SelectShip(int shipType)
+			{
+				ShipSpawner.currentShipType = (ShipType)shipType;
+				ShowLevelSelect();
+			}
 
-	// Select a certain level to load
-	public void SelectLevel(int sceneIndex) {
-		SceneManager.LoadScene(sceneIndex);
-	}
-	public void SelectLevel(string sceneName) {
-		SceneManager.LoadScene(sceneName);
+			// Select a certain level to load
+			public void SelectLevel(int sceneIndex)
+			{
+				SceneManager.LoadScene(sceneIndex);
+				controller.PlayAudio(AudioType.Gameplay_ST, true);
+			}
+			public void SelectLevel(string sceneName)
+			{
+				SceneManager.LoadScene(sceneName);
+				controller.PlayAudio(AudioType.Gameplay_ST, true);
+			}
+		}
+
 	}
 }
--- Assets/Scripts/ReturnToMain.cs
+++ Assets/Scripts/ReturnToMain.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class ReturnToMain : MonoBehaviour
+namespace UnityCore
 {
-	public void loadMainMenu()
+	namespace Audio
 	{
-		SceneManager.LoadScene(0);
+		public class ReturnToMain : MonoBehaviour
+		{
+
+			public AudioController controller;
+
+            private void Start()
+            {
+				controller = GameObject.Find("MusicPlayer").GetComponent<AudioController>();
+            }
+
+            public void loadMainMenu()
+			{
+				SceneManager.LoadScene(0);
+				controller.PlayAudio(AudioType.Menu_ST, true);
+			}
+		}
 	}
 }
+
