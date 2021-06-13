21e5cdabb0f4ab3e623a9c3d9beba81bee1dfb24
alex-smith-26 <85772857+alex-smith-26@users.noreply.github.com>
2021-06-12 20:17:00 -0400

    Kayak level tweaks+Level Progression

---
+++ Assets/Scripts/KayakEndGoal.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class KayakEndGoal : MonoBehaviour
+{
+    [SerializeField] public string nextLevel;
+    public bool hitGoal = false;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ship")
+        {
+            if (!hitGoal)
+            {
+                hitGoal = true;
+                StartCoroutine("NextLevelScene");
+                print("level goal");
+            }
+        }
+    }
+
+    IEnumerator NextLevelScene()
+    {
+        print("loading level " + nextLevel);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(nextLevel);
+        print("loaded " + nextLevel);
+    }
+}
