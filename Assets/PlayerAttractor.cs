fe0a4cbdd863f548a922881e9c72d1c8065b3d66
alex-smith-26 <85772857+alex-smith-26@users.noreply.github.com>
2021-06-13 13:03:12 -0400

    Kayak and Magnet level tweaks

--- Assets/PlayerAttractor.cs
+++ Assets/PlayerAttractor.cs
@@ -8,7 +8,7 @@ public class PlayerAttractor : MonoBehaviour
 
     private List<GameObject> players;
 
-    private float PullPower = 12f;
+    [SerializeField] private float PullPower = 12f;
 
     // Start is called before the first frame update
     void Start()
