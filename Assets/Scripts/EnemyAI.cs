a23d1627211d4ebab8271ea018face197c6da2e0
oberon99 <qpmzwomx@gmail.com>
2021-06-12 22:44:10 -0400

    Misc. code comments, enemies will now shoot at player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -10,16 +10,23 @@ public class EnemyAI : MonoBehaviour
     private GameObject nextNode;
     public GameObject player;
     [SerializeField] public enum AIType {Chase, Kamikaze, Cautious, Patrol};
+    [SerializeField] public GameObject bullet;
     public AIType type;
     public float rotationSpeed;
     public float speed;
     private Rigidbody2D rb2d;
     private Vector2 desiredPosition;
+    private Vector2 lastKnownPlayerLoc;
     int nodeIndex = 0;
+    public float fireCooldown = 0.75f;
+    public float fire = 0f;
+    Vector2 dummy;
 
     void FaceTowards(GameObject go)
     {
+        // Convert the vector between enemy and desired target to a signed angle
         float angleToTarget = Vector2.SignedAngle(new Vector2(0, 1), (go.transform.position - transform.position));
+        // Sanity check - make sure the angle through which the enemy will rotate is less than 180 degrees
         if (Mathf.Abs(rb2d.rotation - angleToTarget) < 180)
         {
             rb2d.rotation -= (rb2d.rotation - angleToTarget) * (rotationSpeed / 100);
@@ -47,15 +54,19 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check for LOS to the player's position
         RaycastHit2D lineOfSight = Physics2D.Raycast(transform.position, (player.transform.position-transform.position));
         Debug.DrawLine(transform.position, player.transform.position, Color.red);
         if(lineOfSight.collider != null)
         {
+            // If player is visible, change boolean flags to match and update player's last known location
             if(lineOfSight.collider.gameObject == player)
             {
                 hasSightlineOnPlayer = true;
                 hasAggroOnPlayer = true;
+                lastKnownPlayerLoc = player.transform.position;
             }
+            // If player can't be seen, change boolean flag and bring enemy to a stop
             else
             {
                 hasSightlineOnPlayer = false;
@@ -70,17 +81,19 @@ public class EnemyAI : MonoBehaviour
                 {
                     case AIType.Chase:
                         {
-
+                            FaceTowards(player);
+                            rb2d.velocity = -((transform.position - player.transform.position).normalized) * speed;
                             break;
                         }
                     case AIType.Cautious:
                         {
+
                             break;
                         }
                     case AIType.Kamikaze:
                         {
                             FaceTowards(player);
-                            rb2d.velocity = ((transform.position - player.transform.position).normalized) * speed;
+                            rb2d.velocity = -((transform.position - player.transform.position).normalized) * speed;
                             break;
                         }
                     case AIType.Patrol:
@@ -90,19 +103,63 @@ public class EnemyAI : MonoBehaviour
                             break;
                         }
                 }
+                if (Time.time > fire)
+                {
+                    fire = Time.time + fireCooldown;
+                    GameObject newbullet = Instantiate(bullet, transform.position + (transform.up.normalized), Quaternion.identity);
+                    newbullet.GetComponent<Rigidbody2D>().velocity = transform.up * 10;
+                }
             }
+            /*else
+            {
+                if(type == AIType.Chase)
+                {
+                    Vector2 pos2 = new Vector2(transform.position.x, transform.position.y);
+                    Vector2 node2 = new Vector2(nextNode.transform.position.x, nextNode.transform.position.y);
+                    
+                    if ((dummy-lastKnownPlayerLoc).magnitude > 0)
+                    {
+                        float nearestDist = -1.0f;
+                        foreach (GameObject i in nodes)
+                        {
+                            Vector2 i2 = new Vector2(i.transform.position.x, i.transform.position.y);
+                            Vector2 v2 = new Vector2(transform.position.x, transform.position.y);
+                            RaycastHit2D foo = Physics2D.Raycast(lastKnownPlayerLoc, (i2-lastKnownPlayerLoc).normalized, (i2-lastKnownPlayerLoc).magnitude);
+                            if (foo.collider == i)
+                            {
+                                print("found a node that sees LKPL at ");
+                                if (nearestDist < 0 || (lastKnownPlayerLoc - i2).magnitude < nearestDist)
+                                {
+                                    RaycastHit2D bar = Physics2D.Raycast(i.transform.position, (v2 - i2).normalized, (v2 - i2).magnitude);
+                                    if (bar.collider == gameObject)
+                                    {
+                                        nextNode = i;
+                                        dummy = lastKnownPlayerLoc;
+                                        print("nextnode updated to location " + nextNode.transform.position.x + " " + nextNode.transform.position.y);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                    rb2d.velocity = -((pos2-node2).normalized) * speed;
+                }
+            }*/
         }
         else
         {
+            // Default behavior when not in combat - only defined for Patrol-type enemies
             if(type == AIType.Patrol)
             {
                 Vector2 nodePos2 = new Vector2(nextNode.transform.position.x, nextNode.transform.position.y);
                 Vector2 Pos2 = new Vector2(transform.position.x, transform.position.y);
+                
                 if ((Pos2-nodePos2).magnitude > 0.05)
                 {
                     FaceTowards(nextNode);
                     rb2d.velocity = (nodePos2 - Pos2).normalized * speed;
                 }
+                // When the target node is reached, change destination to the next one in the list
                 else
                 {
                     nodeIndex++;
